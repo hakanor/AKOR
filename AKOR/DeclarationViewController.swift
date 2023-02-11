d5c4cd19d5ce9111a556007a64946647f025c0a6
hakanor <hakanor99@gmail.com>
2023-02-11 17:41:11 +0300

    Add create button

--- AKOR/DeclarationViewController.swift
+++ AKOR/DeclarationViewController.swift
@@ -12,19 +12,28 @@ class DeclarationViewController: UIViewController {
     // MARK: - Subviews
     
     private lazy var nationalIdTextField = UITextField()
+    private lazy var createButton = UIButton()
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         nationalIdTextField.translatesAutoresizingMaskIntoConstraints = false
+        createButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nationalIdTextField)
+        view.addSubview(createButton)
         nationalIdTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         nationalIdTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         nationalIdTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75).isActive = true
         nationalIdTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
         nationalIdTextField.backgroundColor = .systemBlue
-        // Do any additional setup after loading the view.
+        
+        createButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        createButton.topAnchor.constraint(equalTo: nationalIdTextField.bottomAnchor, constant: 16).isActive = true
+        createButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        createButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        createButton.backgroundColor = .systemBlue
+        
     }
 
 
