28ec23e62cc07e03f078ee0dc170a788077da187
hakanor <hakanor99@gmail.com>
2023-02-13 23:09:00 +0300

    Improvements on NationalIdFormatter

--- AKOR/NationalIdFormatter.swift
+++ AKOR/NationalIdFormatter.swift
@@ -5,10 +5,18 @@
 //  Created by Hakan Or on 13.02.2023.
 //
 
+import Foundation
+
 final class NationalIdFormatter {
     public func format(text: String) -> String {
         if text.count > 11 {
             return String(text.prefix(11))
+        } else if Int(text) == nil {
+            let numericSet = "0123456789"
+            let filteredCharacters = text.filter {
+                return numericSet.contains(String($0))
+            }
+            return String(filteredCharacters)
         }
         return text
     }
