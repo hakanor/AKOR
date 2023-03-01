bf61a67cb507cdf6f927503eceec1fad709f0f2e
hakanor <hakanor99@gmail.com>
2023-03-01 18:20:25 +0300

    Create DeclarationViewModelTests

--- AKORTests/DeclarationViewModelTests.swift
+++ AKORTests/DeclarationViewModelTests.swift
@@ -6,30 +6,61 @@
 //
 
 import XCTest
+@testable import AKOR
 
 final class DeclarationViewModelTests: XCTestCase {
+    private var sut: DeclarationViewModel!
+    private var delegate: MockDeclarationViewModelDelegate!
+    private var declarationService: MockDeclarationService!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        declarationService = MockDeclarationService()
+        sut = DeclarationViewModel(declarationService: declarationService)
+        delegate = MockDeclarationViewModelDelegate()
+        sut.delegate = delegate
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        delegate = nil
+        declarationService = nil
+        try super.tearDownWithError()
+    }
+    
+    func test_create_declaration_should_be_called_when_national_id_is_not_nil() {
+        // Given
+        let nationalId = "12345678910"
+        
+        // When
+        sut.createButtonDidTap(nationalId: nationalId)
+        
+        // Then
+        XCTAssertTrue(delegate.dataFetchDidStartCalled)
+        XCTAssertTrue(declarationService.createDeclarationCalled)
+        XCTAssertEqual(nationalId, declarationService.passedNationalId)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+final class MockDeclarationService: DeclarationServiceProtocol {
+    var createDeclarationCalled = false
+    var passedNationalId: String?
+    
+    func createDeclaration(nationalId: String?) {
+        createDeclarationCalled = true
+        passedNationalId = nationalId
+    }
+    
+    var delegate: AKOR.DeclarationServiceDelegate?
+    
+}
+
+final class MockDeclarationViewModelDelegate: DeclarationViewModelDelegate {
+    var dataFetchDidStartCalled = false
+
+    func dataFetchDidStart() {
+        dataFetchDidStartCalled = true
     }
 
 }
